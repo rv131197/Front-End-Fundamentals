3877d13f73b011892f69f5cd489e9a5ad425b728
Ramya V <ramya@Ramyas-MacBook-Air.local>
2021-12-15 17:47:06 +0530

    input and change events

--- FED/Javascript/app.js
+++ FED/Javascript/app.js
@@ -1,10 +1,5 @@
-const ramya = document.querySelector('#ramya')
-const input = document.querySelector('#dishName')
-const ul = document.querySelector('#dishes')
-ramya.addEventListener('submit', function(e){
-    e.preventDefault()
-    const newLi = document.createElement('li')
-    newLi.innerText = input.value
-    ul.append(newLi)
-    input.value=""
+const ip = document.querySelector('#input')
+
+ip.addEventListener('change', function(e){
+    console.log(e)
 })
