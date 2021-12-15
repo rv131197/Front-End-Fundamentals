231742cd1ad68c27355f051b2d93ed455d4aadc6
Ramya V <ramya@Ramyas-MacBook-Air.local>
2021-12-15 18:04:27 +0530

    Event bubbling

--- FED/Javascript/app.js
+++ FED/Javascript/app.js
@@ -1,5 +1,18 @@
-const ip = document.querySelector('#input')
+const btn = document.querySelector('#change')
+const div = document.querySelector('#container')
 
-ip.addEventListener('change', function(e){
-    console.log(e)
+btn.addEventListener('click', function(e){
+    div.style.backgroundColor = makeRandColor();
+    e.stopPropagation();
 })
+
+div.addEventListener('click', function(){
+    div.classList.toggle('hide');
+})
+
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`
+}
