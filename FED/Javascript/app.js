893d2d2842eb429c32e6457f99016a3ca62c8995
Ramya V <ramya@Ramyas-MacBook-Air.local>
2021-12-15 15:35:57 +0530

    preventDefault

--- FED/Javascript/app.js
+++ FED/Javascript/app.js
@@ -1,25 +1,10 @@
-// const btn = document.querySelector('button')
-// const h1 = document.querySelector('h1')
-
-// btn.onclick = addEventListener('click', function(){
-//     const newColor = RandColor();
-//     document.body.style.backgroundColor = newColor;
-//     h1.innerText = newColor;
-// })
-
-// function RandColor() {
-//     const r = Math.floor(Math.random() * 255);
-//     const g = Math.floor(Math.random() * 255);
-//     const b = Math.floor(Math.random() * 255);
-//     return `rgb(${r}, ${g}, ${b})`
-// }
-
-const ip = document.querySelector('input')
-ip.addEventListener('keydown', function(e) {
-    console.log(e)
+const ramya = document.querySelector('#ramya')
+const input = document.querySelector('#dishName')
+const ul = document.querySelector('#dishes')
+ramya.addEventListener('submit', function(e){
+    e.preventDefault()
+    const newLi = document.createElement('li')
+    newLi.innerText = input.value
+    ul.append(newLi)
+    input.value=""
 })
-
-
-// ip.addEventListener('keyup', function() {
-//     console.log('KEYUP')
-// })
