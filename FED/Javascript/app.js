3f29cddf26fc4dba2d7f07e7f9472eaa229f3555
Ramya V <ramya@Ramyas-MacBook-Air.local>
2021-12-15 14:47:08 +0530

    keyboard events and event object

--- FED/Javascript/app.js
+++ FED/Javascript/app.js
@@ -1,15 +1,25 @@
-const btn = document.querySelector('button')
-const h1 = document.querySelector('h1')
+// const btn = document.querySelector('button')
+// const h1 = document.querySelector('h1')
 
-btn.onclick = addEventListener('click', function(){
-    const newColor = RandColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
+// btn.onclick = addEventListener('click', function(){
+//     const newColor = RandColor();
+//     document.body.style.backgroundColor = newColor;
+//     h1.innerText = newColor;
+// })
+
+// function RandColor() {
+//     const r = Math.floor(Math.random() * 255);
+//     const g = Math.floor(Math.random() * 255);
+//     const b = Math.floor(Math.random() * 255);
+//     return `rgb(${r}, ${g}, ${b})`
+// }
+
+const ip = document.querySelector('input')
+ip.addEventListener('keydown', function(e) {
+    console.log(e)
 })
 
-function RandColor() {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`
-}
+
+// ip.addEventListener('keyup', function() {
+//     console.log('KEYUP')
+// })
