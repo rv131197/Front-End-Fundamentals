879cc63e7a68f3011b1f917649ea854f2c799fb6
Ramya V <ramya@Ramyas-MacBook-Air.local>
2021-12-16 19:36:24 +0530

    creating promises

--- FED/Javascript/app.js
+++ FED/Javascript/app.js
@@ -1,25 +1,28 @@
 
+const fakeRequest = url => {
+    const rand = Math.random();
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            if(rand<0.7){
+                resolve('HURRRAYYY!!')
+            }
+                reject("YYAAYYA, NOOOn WRINGGG!!")
+        }, 1000)
+    })
+}
 
-setTimeout(()=>{
-    document.body.style.backgroundColor = 'red';
-}, 1000)
+fakeRequest('dogs/1')
+.then((data) => {
+    console.log('DONEEE!!!')
+    console.log(data)
+})
+.catch((err)=>{
+    console.log(err)
+})
 
-setTimeout(()=>{
-    document.body.style.backgroundColor = 'green';
-}, 2000)
 
-setTimeout(()=>{
-    document.body.style.backgroundColor = 'blue';
-}, 3000)
 
-setTimeout(()=>{
-    document.body.style.backgroundColor = 'yellow';
-}, 4000)
 
-setTimeout(()=>{
-    document.body.style.backgroundColor = 'pink';
-}, 5000)
 
-setTimeout(()=>{
-    document.body.style.backgroundColor = 'brown';
-}, 6000)
+
+
